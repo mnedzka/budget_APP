b9e1e693c0e6a15c3fecacbb323182e4f1694ad4
Michał Nędzka <michael.nedzka@gmail.com>
2018-05-13 20:25:08 +0200

    Add Parcel, postcss, autoprefixer, change styles to scss, modularize js

---
+++ app.js
@@ -0,0 +1,8 @@
+import './style.scss';
+
+import budgetController from './budgetController';
+import UIController from './UIController';
+
+const controller = ((budgetCtrl, UICtrl) => {
+
+})(budgetController, UIController);
